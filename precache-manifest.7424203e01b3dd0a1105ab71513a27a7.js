7b35c66aba619816994b071b6b1a7675705fe829
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2020-05-09 20:16:58 +0530

    Updates

---
+++ precache-manifest.7424203e01b3dd0a1105ab71513a27a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbd3b69c7ea1293d7647cde4e517f60",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/index.html"
+  },
+  {
+    "revision": "93e6aa563a9939dd476a",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/css/main.f2aab8eb.chunk.css"
+  },
+  {
+    "revision": "d3cf6b486c718b9ff80e",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/js/2.ee8a294d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/js/2.ee8a294d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e6aa563a9939dd476a",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/js/main.b6651704.chunk.js"
+  },
+  {
+    "revision": "8318c1ed277dbfce363c",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/js/runtime-main.cf3d1f0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/https://vijaynandakumarkumar.github.io/to-do-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
